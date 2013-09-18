bdba8513c62fe8189566653d96d892507382c55e
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-09-17 23:22:45 -0400

    Added URI to pins model and forms

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
   belongs_to :user
   has_attached_file :image, styles: {medium: "320x240>"}
   
@@ -11,6 +11,10 @@ class Pin < ActiveRecord::Base
   
   
   
-  
+  def image_remote_url=(url_value)
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+    
+  end
   
 end
